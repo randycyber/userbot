f9dc4f2bae88fb303959855b5f911eefd1ac6246
Randy CyberUB <randycyber@my.id>
2022-07-06 17:02:53 +0000

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -1,4 +1,4 @@
-from telethon import TelegramClinet, events
+from telethon import TelegramClinet, client, events
 
 api_id = 
 api_hash = ''
@@ -8,7 +8,15 @@ rendy = TelegramClient('session', api_id, api_hash)
 @rendy.on(events.NewMessage(outgoing=True, pattern=r\'.ping'))
 async def pingme(event):
   chat = event.get_chat()
-  await rendy.edit_message(chat, "ping......")
+  await rendy.edit_message(event.message, "ping......")
+  
+@rendy.on(events.NewMessage(outgoing=True, pattern=r\'.alive'))
+async def alive(event):
+  chat = event.get_chat()
+  await client.delete_message(chat, event.message)
+  photo = await client.download_profile_photo('me')
+  await client.send_file(chat, file=photo, caption="yes i'am alive")
+  os.remove(photo)
 
 rendy.start()
 rendy.run.until.disconnected()

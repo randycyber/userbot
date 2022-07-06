d869b7bfb42a25db52c79f327b161f48f88a4a91
Randy CyberUB <randycyber@my.id>
2022-07-06 16:30:35 +0000

    tested

--- userbot.py
+++ userbot.py
@@ -1,1 +1,11 @@
-# test
+from tethon import TelegramClinet, events
+
+api_id = 
+api_hash = ''
+
+rendy = TelegramClient('session', api_id, api_hash)
+
+@rendy.on(events.NewMessage(outgoing=True, pattern=r\'.ping'))
+async def pingme(event):
+  chat = event.get_chat()
+  await rendy.edit_message(chat, "ping......")

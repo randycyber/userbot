f8a3415f27a374dce51e8f064a13c48ea7f6c868
Randy CyberUB <randycyber@my.id>
2022-07-06 17:14:51 +0000

    update userbot.py

--- userbot.py
+++ userbot.py
@@ -3,20 +3,25 @@ from telethon import TelegramClinet, client, events
 api_id = 
 api_hash = ''
 
-rendy = TelegramClient('session', api_id, api_hash)
+client = TelegramClient('session', api_id, api_hash)
 
-@rendy.on(events.NewMessage(outgoing=True, pattern=r\'.ping'))
+@client.on(events.NewMessage(outgoing=True, pattern=r\'.ping'))
 async def pingme(event):
-  chat = event.get_chat()
-  await rendy.edit_message(event.message, "ping......")
+   chat = event.get_chat()
+   await rendy.edit_message(event.message, "ping......")
   
-@rendy.on(events.NewMessage(outgoing=True, pattern=r\'.alive'))
+@client.on(events.NewMessage(outgoing=True, pattern=r\'.alive'))
 async def alive(event):
-  chat = event.get_chat()
-  await client.delete_message(chat, event.message)
-  photo = await client.download_profile_photo('me')
-  await client.send_file(chat, file=photo, caption="yes i'am alive")
-  os.remove(photo)
+   chat = event.get_chat()
+   await client.delete_message(chat, event.message)
+   photo = await client.download_profile_photo('me')
+   await client.send_file(chat, file=photo, 
+   caption=
+   "yes i'am alive.\n"
+   "Owner [dev](https://t.me/FFmpegNotInstalled)\n"
+   "Channel [dev](https://t.me/Rendyprojects)\n"
+   )
+   os.remove(photo)
 
-rendy.start()
-rendy.run.until.disconnected()
+client.start()
+client.run.until.disconnected()
